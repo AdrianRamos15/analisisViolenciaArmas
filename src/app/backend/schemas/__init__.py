455fb9613259f538967befaa0934e09f94203150
Adrián Ramos Romero <115927285+AdrianRamos15@users.noreply.github.com>
2024-12-06 14:20:05 +0100

    Create __init__.py

---
+++ src/app/backend/schemas/__init__.py
@@ -0,0 +1,9 @@
+import sys
+
+sys.path.append("src/app/backend/schemas")
+from base import *  # pylint: disable=import-error
+from incident_climate import *  # pylint: disable=import-error
+from incident_combined import *  # pylint: disable=import-error
+from incident_firearm_laws import *  # pylint: disable=import-error
+from incident_population_poverty import *  # pylint: disable=import-error
+from incident_weekend import *  # pylint: disable=import-error
